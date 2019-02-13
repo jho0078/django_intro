165719a13c0cb888e0262fb751dd68d68a9580f7
jho0078 <jho0078@gmail.com>
2019-02-13 02:50:18 +0000

    04_variable routing

--- home/views.py
+++ home/views.py
@@ -13,3 +13,10 @@ def dinner(request):
     menus = ['장조림버터밥', '왕꼬치구이', '팽이버섯된장국', '단무지무침', '매실차']
     pick = random.choice(menus)
     return render(request, 'dinner.html', {'menus' : menus, 'pick' : pick})
+    
+def hello(request, name):
+    return render(request, 'hello.html', {'name' : name })
+    
+def cube(request, number):
+    result = int(number)**3
+    return render(request, 'cube.html', {'number' : number, 'result' : result})
